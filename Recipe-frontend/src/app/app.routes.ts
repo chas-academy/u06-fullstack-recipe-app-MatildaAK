d5ab7965c17315acb74dada8aa13dd442ddb5688
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-03-20 15:33:07 +0100

    new routes

--- Recipe-frontend/src/app/app.routes.ts
+++ Recipe-frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { RecipeAboutComponent } from './components/recipe-about/recipe-about.component';
+import { UserPageComponent } from './components/user-page/user-page.component';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: AppComponent},
+    {path: 'recipe', component: RecipeAboutComponent},
+    {path: 'profile', component: UserPageComponent, canActivate: [authGuard]},
+];
