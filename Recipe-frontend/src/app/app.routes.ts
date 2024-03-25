7b4e5890e77c851dd177b7998315565fb56ee124
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-03-25 19:05:31 +0100

    added path's

--- Recipe-frontend/src/app/app.routes.ts
+++ Recipe-frontend/src/app/app.routes.ts
@@ -3,9 +3,13 @@ import { AppComponent } from './app.component';
 import { RecipeAboutComponent } from './components/recipe-about/recipe-about.component';
 import { UserPageComponent } from './components/user-page/user-page.component';
 import { authGuard } from './guards/auth.guard';
+import { LoginStatusComponent } from './components/login-status/login-status.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-    {path: '', component: AppComponent},
     {path: 'recipe', component: RecipeAboutComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
     {path: 'profile', component: UserPageComponent, canActivate: [authGuard]},
 ];
