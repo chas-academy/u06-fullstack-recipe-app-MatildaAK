7c76f4fa91da4817896fd40d15b7e81c6b258e65
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-03-27 09:08:12 +0100

    recipe

---
+++ Recipe-frontend/src/app/pages/recipe/recipe.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+
+@Component({
+  selector: 'app-recipe',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipe.component.html',
+  styleUrl: './recipe.component.css'
+})
+export class RecipeComponent implements OnInit {
+  id?: string;
+
+  constructor(private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.id = String(params.get('id'));
+    })
+  }
+  
+}
