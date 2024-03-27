5063efffa27f024b65978e06f5ced095aff02b45
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-03-27 09:08:56 +0100

    router

--- Recipe-frontend/src/app/app.routes.ts
+++ Recipe-frontend/src/app/app.routes.ts
@@ -1,15 +1,13 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { RecipeAboutComponent } from './components/recipe-about/recipe-about.component';
-import { UserPageComponent } from './components/user-page/user-page.component';
+import { RecipesComponent } from './pages/recipes/recipes.component';
+import { RecipeComponent } from './pages/recipe/recipe.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 import { authGuard } from './guards/auth.guard';
-import { LoginStatusComponent } from './components/login-status/login-status.component';
-import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/login/login.component';
+import { LoginTComponent } from './pages/login-t/login-t.component';
 
 export const routes: Routes = [
-    {path: 'recipe', component: RecipeAboutComponent},
-    {path: 'login', component: LoginComponent},
-    {path: 'register', component: RegisterComponent},
-    {path: 'profile', component: UserPageComponent, canActivate: [authGuard]},
+    {path:'', component:RecipesComponent},
+    {path:'recipe/:id', component:RecipeComponent},
+    {path:'profile', component:ProfileComponent, canActivate: [authGuard]},
+    {path:'login', component:LoginTComponent}
 ];
