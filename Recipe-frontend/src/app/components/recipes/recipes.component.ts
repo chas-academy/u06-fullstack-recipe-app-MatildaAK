10efa68e334dd10782d0a03fbe223a19a75da555
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-04-01 12:36:15 +0200

    recipes

---
+++ Recipe-frontend/src/app/components/recipes/recipes.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../../interfaces/recipe';
+import { RecipeService } from '../../services/recipe.service';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { RecipeidformatterPipe } from "../../pipes/recipeidformatter.pipe";
+
+@Component({
+    selector: 'app-recipes',
+    standalone: true,
+    templateUrl: './recipes.component.html',
+    styleUrl: './recipes.component.css',
+    imports: [FormsModule, RouterLink, RecipeidformatterPipe]
+})
+export class RecipesComponent {
+  recipes?: Recipe[];
+
+  searchterm = 'Chicken';
+
+  constructor(private recipeService: RecipeService) {}
+
+  searchRecipe() {
+    this.recipeService.getRecipes(this.searchterm).subscribe((result) => {
+      console.log(result);
+      let recipes: Recipe[];
+      recipes = result.hits.map((item: { recipe: { label: any; image: any; ingredientLines: any; totalTime: any; }; _links: { self: { href: any; }; }; }) => {
+        return {
+          label: item.recipe.label,
+          image: item.recipe.image,
+          ingredientLines: item.recipe.ingredientLines,
+          totalTime: item.recipe.totalTime,
+          selfref: item._links.self.href,
+        };
+      });
+      console.table(recipes);
+      this.recipes = recipes;
+    });
+  }
+}
