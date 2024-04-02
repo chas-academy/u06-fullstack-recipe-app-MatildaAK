633f7a93133f1f8bc22e187ea0c8de8f40c216f0
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-04-02 12:24:20 +0200

    trying to add and show recipe

--- Recipe-frontend/src/app/components/recipe/recipe.component.ts
+++ Recipe-frontend/src/app/components/recipe/recipe.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Recipe } from '../../interfaces/recipe';
+import { RecipeService } from '../../services/recipe.service';
 
 @Component({
   selector: 'app-recipe',
@@ -11,12 +13,26 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 export class RecipeComponent implements OnInit {
   id?: string;
 
-  constructor(private route: ActivatedRoute){}
+  recipe: Recipe | undefined;
+
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService){}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params: ParamMap) => {
       this.id = String(params.get('id'));
-    })
+      if (this.id) {
+        // Assuming you have a method in RecipeService to get recipe by ID
+        this.recipeService.getRecipe(this.id).subscribe((recipe: Recipe | undefined) => {
+          if(recipe){
+            this.recipe = recipe;
+          } else {
+            console.error('Recipe not found');
+          }
+          
+        });
+      }
+    });
   }
+
   
 }
