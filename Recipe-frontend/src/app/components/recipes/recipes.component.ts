3a5cee7ee5573c30a2361096a1cc54ab43f624a3
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-04-05 18:41:42 +0200

    added filter and rerouting

--- Recipe-frontend/src/app/components/recipes/recipes.component.ts
+++ Recipe-frontend/src/app/components/recipes/recipes.component.ts
@@ -1,26 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../../interfaces/recipe';
 import { RecipeService } from '../../services/recipe.service';
 import { FormsModule } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { RecipeidformatterPipe } from "../../pipes/recipeidformatter.pipe";
+import { CommonModule } from '@angular/common';
+import { Filter } from '../../interfaces/filter';
 
 @Component({
     selector: 'app-recipes',
     standalone: true,
     templateUrl: './recipes.component.html',
     styleUrl: './recipes.component.css',
-    imports: [FormsModule, RouterLink, RecipeidformatterPipe]
+    imports: [FormsModule, RouterLink, RecipeidformatterPipe, CommonModule]
 })
-export class RecipesComponent {
+export class RecipesComponent implements OnInit {
   recipes?: Recipe[];
 
-  searchterm = 'Chicken';
+  filter: Filter ={
+    query: '',
+    mealTypes: '',
+    health: '',
+    cuisineTypes: '',
+    dishType: ''
+  }
 
-  constructor(private recipeService: RecipeService, private router: Router) {}
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void{
+    this.route.url.subscribe(url => {  
+      console.log(url);
+      console.log(url[0]);   
+        if (url[0] !== undefined && url[0].path === 'recipes') {
+          this.searchRecipe();
+        }
+    })
+  }
 
   searchRecipe() {
-    this.recipeService.getRecipes(this.searchterm).subscribe((result) => {
+    this.recipeService.getRecipes(this.filter).subscribe((result) => {
       console.log(result);
       let recipes: Recipe[];
       recipes = result.hits.map((item: { recipe: { label: any; image: any; ingredientLines: any; totalTime: any; }; _links: { self: { href: any; }; }; }) => {
