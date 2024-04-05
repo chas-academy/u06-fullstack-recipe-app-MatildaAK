2c301e6e9e1e1c69c8b5b27511c16308e11cce19
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-04-05 18:43:21 +0200

    changed routes

--- Recipe-frontend/src/app/app.routes.ts
+++ Recipe-frontend/src/app/app.routes.ts
@@ -5,10 +5,12 @@ import { authGuard } from './guards/auth.guard';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 export const routes: Routes = [
-    {path:'', component:RecipesComponent},
+    {path:'', component:HomeComponent},
+    {path:'recipes', component:RecipesComponent},
     {path:'recipe/:id', component:RecipeComponent},
     {path:'profile', component:ProfileComponent, canActivate: [authGuard]},
     {path:'login', component:LoginComponent},
