22d95ed5715a976d218466e8c791a8cfde96c553
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-04-05 18:39:26 +0200

    home component, function and styling

---
+++ Recipe-frontend/src/app/components/home/home.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from '../../services/recipe.service';
+import { Router, RouterLink } from '@angular/router';
+import { RecipesComponent } from '../recipes/recipes.component';
+import { Recipe } from '../../interfaces/recipe';
+import { RecipeidformatterPipe } from '../../pipes/recipeidformatter.pipe';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Filter } from '../../interfaces/filter';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RecipesComponent, FormsModule, RouterLink, RecipeidformatterPipe, CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements OnInit{
+  recipes?: Recipe[];
+
+  filter: Filter ={
+    query: '',
+    mealTypes: '',
+    health: '',
+    cuisineTypes: '',
+    dishType: 'Desserts'
+  }
+
+  constructor(private recipeService: RecipeService, private router: Router){}
+
+  ngOnInit(): void {
+    // this.mealType(); 
+  }
+
+  mealType() {
+    this.recipeService.getRecipes(this.filter).subscribe((result) => {
+      console.log(result);
+      let recipes: Recipe[];
+      recipes = result.hits.map((item: { recipe: { label: any; image: any; ingredientLines: any; totalTime: any; }; _links: { self: { href: any; }; }; }) => {
+        return {
+          label: item.recipe.label,
+          image: item.recipe.image,
+          ingredientLines: item.recipe.ingredientLines,
+          totalTime: item.recipe.totalTime,
+          selfref: item._links.self.href,
+        };
+      });
+      console.table(recipes);
+      this.recipes = recipes;
+    });
+  }
+}
