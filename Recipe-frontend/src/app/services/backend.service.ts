9487ea5fcc109fd97c5282b7eb34d93371674895
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-04-07 20:33:06 +0200

    removed unused code and import

--- Recipe-frontend/src/app/services/backend.service.ts
+++ Recipe-frontend/src/app/services/backend.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
-import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +17,4 @@ export class BackendService {
     return this.http.post('https://u06-fullstack-recipe-app-matildaak.onrender.com/api/login', data);
   }
 
-  // getUser(data:any){
-  //   return this.http.get('https://u06-fullstack-recipe-app-matildaak.onrender.com/api/getuser', data)
-  // }
 }
